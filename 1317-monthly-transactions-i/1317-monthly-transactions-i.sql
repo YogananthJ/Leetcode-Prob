78f4ba1defd918786e69d45f3766920f88ddbe66
YOGANANTH J <mjyogananth@gmail.com>
2025-04-20 21:27:36 +0530

    Time: 593 ms (79.61%), Space: 0B (100%) - LeetHub

---
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -0,0 +1,2 @@
+select Left(trans_date,7) as month ,country as country ,count(state) as trans_count , sum(state='approved') as approved_count , sum(amount) as trans_total_amount , sum((state = 'approved' )*amount ) as approved_total_amount from Transactions
+group by month,country;
