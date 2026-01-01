278e3e6ff63a14ccf20a7c9be67ef9e9001161aa
YOGANANTH J <mjyogananth@gmail.com>
2026-01-01 11:54:08 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.02%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,23 +1,22 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-
-    int n = digits.length;
-
-    // Loop from last digit backwards
-    for (int i = n - 1; i >= 0; i--) {
-
-        digits[i]++;          // increment the digit
-        if (digits[i] < 10) {  // no carry
-            return digits;     // done
-        }
-
-        digits[i] = 0; // digit was 9, now becomes 0 (carry continues)
+        int n = digits.length;
+       
+        // digits[n-1]=digits[n-1]+1;
+        for (int i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+        digits[i]++;
+        return digits;
     }
-
-    // If we reach here: all digits were 9
-    int[] newDigits = new int[n + 1];
-    newDigits[0] = 1; // Example: 999 + 1 = 1000
-    return newDigits;
+    digits[i] = 0;
 }
 
+
+int[] result = new int[digits.length + 1];
+result[0] = 1;
+return result;
+
+        
+       
+    }
 }
