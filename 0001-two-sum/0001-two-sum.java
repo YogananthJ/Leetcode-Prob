e4023add12f8a175356bcc2f22bca4eef165f051
YOGANANTH J <mjyogananth@gmail.com>
2026-01-31 11:17:01 +0530

    Time: 49 ms (9.96%), Space: 47.1 MB (63.49%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,11 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int k=1, t=0;;
+        int[] index = new int[2];
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
                 if(nums[i]+nums[j]==target){
-                    return new int[]{i,j};
+                    index[t]=i;
+                    index[k]=j;
                 }
             }
-        } return new int[]{};
-    } 
+        }
+        return index;
+    }
 }
