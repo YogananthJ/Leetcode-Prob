9b2c63fbdc3eb7cfd062273c90e8c2031c8cd02d
YOGANANTH J <mjyogananth@gmail.com>
2026-03-06 09:41:31 +0530

    Time: 1 ms (62.25%), Space: 43.3 MB (33.21%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,15 +1,13 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int count =0;
-        for(int i=0;i<jewels.length();i++){
-            for(int j=0;j<stones.length();j++){
-                char a = jewels.charAt(i);
-                char b = stones.charAt(j);
-                if(a==b){
-                    count++;
+        int c=0;
+        for(int i=0;i<stones.length();i++){
+            for(int j=0;j<jewels.length();j++){
+                if(jewels.charAt(j)==stones.charAt(i)){
+                    c++;
                 }
             }
         }
-        return count;
+        return c;
     }
 }
