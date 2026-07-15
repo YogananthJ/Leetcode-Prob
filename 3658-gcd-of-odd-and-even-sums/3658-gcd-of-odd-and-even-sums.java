6b13c29115768cedeabbd1296f85cac1d43e446a
YOGANANTH J <mjyogananth@gmail.com>
2026-07-15 11:20:24 +0530

    Time: 0 ms (100%), Space: 42.9 MB (6.12%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,16 +1,16 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int eve = 0, odd = 0;
-        for(int i=1;i<=n+n;i++){
-            if(i%2==0){
-                eve+=i;
-            }
-            else
-            {
-                odd+=i;
-            }
-        }
+        // int eve = 0, odd = 0;
+        // for(int i=1;i<=n+n;i++){
+        //     if(i%2==0){
+        //         eve+=i;
+        //     }
+        //     else
+        //     {
+        //         odd+=i;
+        //     }
+        // }
 
-        return eve-odd;
+        return n;
     }
 }
