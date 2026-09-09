4a84e9197a4350e7f80444ae3d3af43544a08cc9
YOGANANTH J <mjyogananth@gmail.com>
2026-09-09 16:17:04 +0530

    Time: 1 ms (99.36%), Space: 42.7 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -11,52 +11,40 @@ class Solution {
         if (l < 4) {
             return 0;
         } else if (l < 7) {
-            // 1,000 → n
             return (n - 1_000L) + 1;
         } else if (l < 10) {
-            // Complete 1-comma range
             r += a;
 
-            // 1,000,000 → n
             long e = (n - 1_000_000L) + 1;
             r += e * 2;
 
             return r;
         } else if (l < 13) {
-            // Complete 1-comma range
             r += a;
 
-            // Complete 2-comma range
             r += b;
 
-            // 1,000,000,000 → n
             long e = (n - 1_000_000_000L) + 1;
             r += e * 3;
 
             return r;
         } else if (l < 16) {
-            // Complete 1-comma range
             r += a;
 
-            // Complete 2-comma range
             r += b;
 
-            // Complete 3-comma range
             r += c;
 
-            // 1,000,000,000,000 → n
             long e = (n - 1_000_000_000_000L) + 1;
             r += e * 4;
 
             return r;
         } else {
-            // Complete 1, 2, 3 and 4 comma ranges
             r += a;
             r += b;
             r += c;
             r += d;
 
-            // 1,000,000,000,000,000 → n
             long e = (n - 1_000_000_000_000_000L) + 1;
             r += e * 5;
 
